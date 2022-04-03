0ff6cf950b0875e0284157a4d5f7766ab04015ec
Florian Maas <florian.maas@ingka.ikea.com>
2022-04-03 20:19:59 +0200

    changed plot titles to enable dynamic resizing

--- src/components/MyPlotly.js
+++ src/components/MyPlotly.js
@@ -23,24 +23,46 @@ const Plotly = Loadable({
   timeout: 10000,
 })
 
-export const MyPlotly = ({ data, layout, style, config, ...rest }) =>
-{
-    let {width, ...newlayout} = layout
-    return (<Plotly
-      data={data}
-      layout={{
-        ...newlayout,
-        paper_bgcolor: 'rgba(0,0,0,0)',
-        plot_bgcolor: 'rgba(0,0,0,0)',
-        autosize: true
-      }}
-      style={{ width: `100%`, ...style }}
-      useResizeHandler
-      config={{
-        displayModeBar: false,
-        showTips: false,
-        ...config,
-      }}
-    ></Plotly>
-    )
+/**
+ * Take the data and layout of an existing plotly graph, and turn it into a reactive plot for use in a blog post.
+ * More specifically:
+ *  - The plot width is ignored, and instead the plot is configured for dynamic resizing.
+ *  - The title is removed from the plot and added as a h3 above the plot to enable dynamic resizing of the title.
+ *  - The background is removed from the plot.
+ *  - The margin is changed to preset values.
+ */
+export const MyPlotly = ({ data, layout, style, config, ...rest }) => {
+  let { width, margin, title, ...newlayout } = layout
+
+  newlayout = {
+    ...newlayout, margin: {
+      l: 30,
+      r: 30,
+      b: 80,
+      t: 20,
+    }
+  }
+  console.log(title.text)
+  return (
+    <div>
+      <h3 className={plotlyStyles.plotTitle}> {title.text} </h3>
+
+      <Plotly
+        data={data}
+        layout={{
+          ...newlayout,
+          paper_bgcolor: 'rgba(0,0,0,0)',
+          plot_bgcolor: 'rgba(0,0,0,0)',
+          autosize: true
+        }}
+        style={{ width: `100%`, ...style }}
+        useResizeHandler
+        config={{
+          displayModeBar: false,
+          showTips: false,
+          ...config,
+        }}
+      ></Plotly>
+    </div>
+  )
 }
