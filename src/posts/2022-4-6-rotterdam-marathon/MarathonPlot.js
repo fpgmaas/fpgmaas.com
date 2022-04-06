3b8e266633bba1afa6fddd09574f60c7c9420c00
Florian Maas <florian.maas@ingka.ikea.com>
2022-04-06 14:29:02 +0200

    working with timeline, but slow

--- src/posts/2022-4-6-rotterdam-marathon/MarathonPlot.js
+++ src/posts/2022-4-6-rotterdam-marathon/MarathonPlot.js
@@ -2,6 +2,31 @@ import React, { useState, useEffect } from "react";
 import Slider from 'react-input-slider';
 import * as marathonStyles from './marathon.module.scss'
 
+import { MyPlotly } from '../../components/MyPlotly'
+
+function calculate_current_distance_of_athlete(splits, time) {
+  var current_pos;
+  const current_split_index = splits.findIndex(split => {
+    return split['time'] > time;
+  });
+  if (current_split_index == 0) {
+    // still in the first split.
+    current_pos = splits[0]['distance'] * (time / splits[0]['time'])
+  } else if (current_split_index != -1) {
+    // between first split and finish.
+    var start_dist = splits[current_split_index - 1]['distance'];
+    var total_time_current_segment = splits[current_split_index]['time'] - splits[current_split_index - 1]['time'];
+    var total_dist_current_segment = splits[current_split_index]['distance'] - splits[current_split_index - 1]['distance'];
+    var time_elapsed_current_segment = time - splits[current_split_index - 1]['time'];
+    current_pos = start_dist + total_dist_current_segment * (time_elapsed_current_segment / total_time_current_segment);
+  } else {
+    // finished.
+    current_pos = splits.slice(-1)[0]['distance']
+  }
+  return current_pos;
+}
+
+
 function MarathonPlot() {
   const githubUrl = "https://raw.githubusercontent.com/fpgmaas/rotterdam-marathon/python/data/6855879561074155264_480016.json";
 
@@ -20,38 +45,41 @@ function MarathonPlot() {
   };
 
   useEffect(() => {
-
-    if (jsonData.length >0)
-    {
-    var current_pos;
-    const splits = jsonData[0].splits
-    const current_split_index = splits.findIndex(split => {
-      return split['time'] > time;
-    });
-    if(current_split_index==0){
-      current_pos = splits[0]['distance'] * (time/splits[0]['time'])
-      console.log('current_pos')
-      console.log(current_pos)
-    } else if(current_split_index!=-1) {
-      var start_dist = splits[current_split_index-1]['distance'];
-      var total_time_current_segment = splits[current_split_index]['time'] - splits[current_split_index-1]['time'];
-      var total_dist_current_segment = splits[current_split_index]['distance'] - splits[current_split_index-1]['distance'];
-      var time_elapsed_current_segment = time - splits[current_split_index-1]['time'];
-      current_pos = start_dist + total_dist_current_segment * (time_elapsed_current_segment/total_time_current_segment);
-    } else
-    {
-      current_pos = splits.slice(-1)[0]['distance']
+    if (jsonData.length > 0) {
+      setCurrentPositions(
+        jsonData.map(athlete => {
+          return {
+            bib: athlete.bib,
+            distance: calculate_current_distance_of_athlete(athlete.splits, time)
+          }
+        })
+      )
     }
-    setCurrentPositions(current_pos)
+  }, [jsonData, time])
+
+
+  function get_plot_data(currentPositions) {
+    if (currentPositions.length > 0) {
+      return [{
+        y: currentPositions.map(e => { return e.distance }),
+        x: currentPositions.map(e => { return time }),
+        mode: 'markers',
+        type: 'scatter',
+        marker: {
+          color: 'rgb(17, 157, 255)',
+          size: 1,
+        }
+      }]
+    }
   }
-  },[jsonData, time])
 
   return (
-    <div className = {marathonStyles.myDiv}>
+    <div className={marathonStyles.myDiv}>
       ({time})
-      ({JSON.stringify(currentPositions)})
-      <Slider xmin = {0} xmax = {10*60*60} axis="x" x={time} onChange={({ x }) => setTime(x)} />
+      <Slider xmin={0} xmax={10 * 60 * 60} axis="x" x={time} onChange={({ x }) => setTime(x)} />
+      ({JSON.stringify(currentPositions[0])})
       {jsonData ? JSON.stringify(jsonData[0]) : 'hello'}
+      <MyPlotly data={get_plot_data(currentPositions)} layout={{height: 1000}}></MyPlotly>
     </div>
   );
 }
